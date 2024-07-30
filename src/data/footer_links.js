5ab1a6268df6ca0e6660ceeb5d1ead749d0e2a59
andrearomeo04 <pisco004@gmail.com>
2024-07-31 01:59:04 +0200

    [UPD] modified properties of each object

--- src/data/footer_links.js
+++ src/data/footer_links.js
@@ -11,37 +11,34 @@ const links_1 = [
 export {links_1}
 
 const links_2 = [
-    { text: 'Characters' },
-    { text: 'Comics' },
-    { text: 'Movies' },
-    { text: 'TV' },
-    { text: 'Games' },
-    { text: 'Videos' },
-    { text: 'News' }
+    { text: 'Shop DC' },
+    { text: 'Shop DC Collectibles' },
 ]
 
 export {links_2}
 
 const links_3 = [
-    { text: 'Characters' },
-    { text: 'Comics' },
-    { text: 'Movies' },
-    { text: 'TV' },
-    { text: 'Games' },
-    { text: 'Videos' },
-    { text: 'News' }
+    { text: 'Terms Of Use' },
+    { text: 'Privacy policy (New)' },
+    { text: 'Ad choices' },
+    { text: 'Advertising' },
+    { text: 'Jobs' },
+    { text: 'Subscriptions' },
+    { text: 'Talent Workshops' },
+    { text: 'CPSC Certificates' },
+    { text: 'Ratings' },
+    { text: 'Shop Help' },
+    { text: 'Contact Us' },
 ]
 
 export {links_3}
 
 const links_4 = [
-    { text: 'Characters' },
-    { text: 'Comics' },
-    { text: 'Movies' },
-    { text: 'TV' },
-    { text: 'Games' },
-    { text: 'Videos' },
-    { text: 'News' }
+    { text: 'DC' },
+    { text: 'MAD Magazines' },
+    { text: 'DC Kids' },
+    { text: 'DC Universe' },
+    { text: 'DC Power Visa' },
 ]
 
 export {links_4}
