ca65146ef7b2e51f1dbdc080775d73cf08726a41
andrearomeo04 <pisco004@gmail.com>
2024-07-30 17:29:13 +0200

    [UPD] completed macrostructure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,11 +5,74 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        
+    <div id="background">
+        <div class="container">
+            <div class="row">
+                <div class="col-8 d-flex">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-4 d-flex">
+                    <div class="logo-footer">
+                        <img src="../assets/img/dc-logo-bg.png" alt="logo">
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+    <div id="bg-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-8 d-flex">
+                    
+                </div>
+                <div class="col-4 d-flex">
+                    
+                </div>
+            </div>
+        </div> 
+    </div>
+    <div id="bg-black">
+        <div class="container">
+            <div class="row">
+                <div class="col-8 d-flex">
+                    
+                </div>
+                <div class="col-4 d-flex">
+                    
+                </div>
+            </div>
+        </div> 
     </div>
 </template>
 
-<style lang="">
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+
+    #background {
+        background-image: url(../assets/img/footer-bg.jpg);
+    }
+    
+    .logo-footer {
+        img {
+            width: 400px;
+            object-fit: contain;
+            overflow: hidden;
+        }
+    }
+
+    #bg-gray {
+        background-color: $footer-color;
+        height: 100px;
+    }
+
+    #bg-black {
+        background-color: black;
+        height: 80px;
+    }
     
 </style>
