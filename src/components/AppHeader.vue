0e83dd7071d7e92c76691f2c8d938231b11bbebc
andrearomeo04 <pisco004@gmail.com>
2024-07-30 11:34:43 +0200

    [UPD] created structure of header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,20 @@ export default {
 
 <template lang="">
     <div>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-5 bg-primary">
+                    <div class="py-4">
+                        <img src="../assets/img/dc-logo.png" alt="logo">
+                    </div>
+                </div>
+                <div class="col-7 bg-primary">
+                    <div class="py-5">
+                        
+                    </div>
+                </div>
+            </div>
+        </div> 
     </div>
 </template>
 <style scoped>
