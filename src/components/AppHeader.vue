2999746d215a2cf9826697d300d06b354ebbceb5
andrearomeo04 <pisco004@gmail.com>
2024-07-30 22:42:56 +0200

    [UPD] created data folder where there'll be js files that contain array of objects and added dinamically the top right menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,11 @@
 <script>
+import { mainMenu } from '../data/menues'
 export default {
-            
+         data () {
+            return {
+                mainMenu
+            }
+         } 
 }
 </script>
 
@@ -8,20 +13,29 @@ export default {
     <div>
         <div class="container">
             <div class="row">
-                <div class="col-5">
-                    <div class="py-4">
+                <div class="col-5 d-flex align-items-center py-4">
+                    <div class="">
                         <img src="../assets/img/dc-logo.png" alt="logo">
                     </div>
                 </div>
-                <div class="col-7">
-                    <div class="py-5">
-                        
-                    </div>
+                <div class="col-7 d-flex align-items-center">
+                        <ul class="list-unstyled d-flex mb-0">
+                            <li v-for="(items, index) in mainMenu" :key="index" class="mx-3">
+                                {{ items.text }}
+                            </li>
+                        </ul>
                 </div>
             </div>
         </div> 
     </div>
 </template>
-<style scoped>
-    
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *;
+
+    ul {
+        li {
+            font-size: $font-1;
+        }
+    }
+
 </style>
