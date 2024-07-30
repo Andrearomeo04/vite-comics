64525c2a3260d8a1ab8c20fd9ebd6bd23c972891
andrearomeo04 <pisco004@gmail.com>
2024-07-30 10:33:07 +0200

    [UPD] imported AppHeader component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue'
 
+export default {
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
   <div>
-
+    <header>
+      <AppHeader />
+    </header>
   </div>
 </template>
 
