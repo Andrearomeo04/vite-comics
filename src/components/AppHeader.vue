5555cfb644d092283136ae620dfc0013258b7e72
andrearomeo04 <pisco004@gmail.com>
2024-07-30 23:02:28 +0200

    [UPD] added ternary operator that add active class to the active element

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,14 +13,14 @@ export default {
     <div>
         <div class="container">
             <div class="row">
-                <div class="col-5 d-flex align-items-center py-4">
+                <div class="col-5 d-flex align-items-center">
                     <div class="">
                         <img src="../assets/img/dc-logo.png" alt="logo">
                     </div>
                 </div>
                 <div class="col-7 d-flex align-items-center">
                         <ul class="list-unstyled d-flex mb-0">
-                            <li v-for="(items, index) in mainMenu" :key="index" class="mx-3">
+                            <li v-for="(items, index) in mainMenu" :key="index" class="mx-3" :class="items.active ? 'active' : ''">
                                 {{ items.text }}
                             </li>
                         </ul>
@@ -35,7 +35,12 @@ export default {
     ul {
         li {
             font-size: $font-1;
+            cursor: pointer;
+            padding: $padding-1 0px;
         }
+    }
+    .active {
+        border-bottom: $active;
     }
 
 </style>
