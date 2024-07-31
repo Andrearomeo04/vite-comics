17f66c4d90171591ad80fa35a0bb577366659994
andrearomeo04 <pisco004@gmail.com>
2024-07-31 05:30:26 +0200

    [UPD] added jumbotron and added position fixed to header

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,6 +1,8 @@
 <script>
 import { icons } from '../data/icons'
+import moviesCard from './moviesCard.vue'
 export default {
+    components: moviesCard,
     data () {
             return {
                 icons
@@ -10,22 +12,27 @@ export default {
 </script>
 
 <template lang="">
-    <div class="bg-black">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-white py-5">
-                    <h4>Content goes here</h4>
+    <div class="content">
+        <div class="jumbo">
+            <img src="../assets/img/jumbotron.jpg">
+        </div>
+        <div class="bg-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-white py-5">
+                        <h4>Content goes here</h4>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="bg-primary">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-white py-5 d-flex justify-content-around">
-                    <div v-for="(icon, index) in icons" :key="index" class="d-flex align-items-center inside">
-                        <img :src="icon.img" />
-                        <a href="#" class="ms-5 mb-0 text-white text-decoration-none">{{ icon.text }}</a>
+        <div class="bg-primary">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-white py-5 d-flex justify-content-around">
+                        <div v-for="(icon, index) in icons" :key="index" class="d-flex align-items-center inside">
+                            <img :src="icon.img" />
+                            <a href="#" class="ms-5 mb-0 text-white text-decoration-none">{{ icon.text }}</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -36,9 +43,21 @@ export default {
 <style lang="scss" scoped>
     @use '../styles/partials/variables' as *;
 
+    .content {
+        margin-top: 130px;
+    }
+
     .inside {
         img {
             width: 50px;
+        }
+    }
+    .jumbo {
+        img {
+            object-fit: cover;
+            object-position: top;
+            width: 100%;
+            height:400px;
         }
     }
 </style>
