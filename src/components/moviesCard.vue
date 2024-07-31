b7bb2bd9f1cebaa589b28b59652d9a68fe28e11b
andrearomeo04 <pisco004@gmail.com>
2024-07-31 06:01:33 +0200

    [FIX] added cards structure in moviesCard

--- src/components/moviesCard.vue
+++ src/components/moviesCard.vue
@@ -1,13 +1,29 @@
 <script>
 export default {
-    
+    props: {
+        thumb: string,
+        series: string,
+        price: string,
+        type: string
+    },
 }
 
 </script>
 
 <template lang="">
     <div>
-        
+        <div class="container">
+                <div class="row">
+                    <div class="col-2 text-white py-5">
+                        <div class="thumb">
+                            <img :src="thumb">
+                        </div>
+                        <h6>{{ series }}</h6>
+                        <h6>{{ price }}</h6>
+                        <h6>{{ type }}</h6>
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 
